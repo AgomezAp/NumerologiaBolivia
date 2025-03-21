926c994332df0dd53b23663baeac9da9b0d26888
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2025-03-21 16:15:19 -0500

    Add initial lodash and moment files for dependency management

---
+++ NumerologiaFrontBolivia/src/app/interfaces/datos.ts
@@ -0,0 +1,9 @@
+export interface Datos{
+    Nombre: string;
+    fecha_nacimiento:Date;
+    genero: string;
+    estado_animo: string;
+    numero_suerte: string;
+    telefono: string;
+    pais: string;
+}
